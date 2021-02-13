eb17e9f278f09849f456b8f5749bbba5ac07564b
Luke Bergmann <lukebergmann0@gmail.com>
2021-02-13 18:55:04 +0000

    Used for loop to create chorus line and conditionals file

---
+++ conditionals.js
@@ -0,0 +1,11 @@
+const temperature = 13
+
+if (temperature < 0) {
+  console.log("Brr, Grab a jacket!");
+} else if (temperature < 15) {
+  console.log("Not bad, a longsleeve will do!");
+} else {
+  console.log("This is definitely t-shirt weather!")
+}
+
+console.log(temperature);
